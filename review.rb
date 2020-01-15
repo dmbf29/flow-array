8b2db17ff5bd51ea29fba578456c4c115c9559a6
Douglas Berkley <douglasmberkley@gmail.com>
2020-01-15 12:58:45 +0900

    added review.rb

---
+++ review.rb
@@ -0,0 +1,31 @@
+age = 28
+# name of variable
+# = assignment
+# value
+
+# increment
+age += 1
+age = age + 1
+
+# interpolation
+puts "Arnaud is #{age} years old"
+
+# concatenation
+"Arnaud is " + age.to_s + " years old"
+
+# return booleans
+12.even?
+
+# bang operator
+name = 'rich'
+name.capitalize!
+# override the original value
+
+# puts # gives a new line at the end
+# print # doesnt
+# p ["a", "b", "c"]
+
+# get data from the user
+gets.chomp
+# always gives a string
+
